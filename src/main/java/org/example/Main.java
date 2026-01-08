08d2e34ac833eff031448fb436c4731c6fed4d7b
Mario Jasso <jasso.munoz.mario.alberto@gmail.com>
2026-01-08 08:34:30 -0600

    Commit Main Mario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Calculadora calculadora = new Calculadora();
+        Presentacion presentacion = new Presentacion();
+
+        do {
+            
+        }
 
     }
 }
